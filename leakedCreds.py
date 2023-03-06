a280580a84aa39fa2532f74991e69fb41786e1fa
ViniciusBrenny <126772682+ViniciusBrenny@users.noreply.github.com>
2023-03-06 14:26:38 +0000

    Add files via upload

---
+++ leakedCreds.py
@@ -0,0 +1,4 @@
+"AadClientId": "11451261-d963-4678-9875-adab8030b06e",
+  "AadSecret": "UC88Q~A5uYs7Z8xjiIWA03UYD0mK.oY7KtliFbgP",
+  "AadTenantDomain": "vbrennydemo.onmicrosoft.com",
+  "AadTenantId": "af8038f4-7f5f-439d-afec-7a3ac41b7735",
